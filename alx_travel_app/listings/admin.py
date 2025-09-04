9ccd0ebaa8410da652356f76b324689d2f6b6e93
elainees-devs <elainees.devs@gmail.com>
2025-09-04 18:48:28 +0300

    Register Booking, Listing, and Payment models in admin

--- alx_travel_app/listings/admin.py
+++ alx_travel_app/listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Booking, Listing, Payment
+
+admin.site.register(Booking)
+admin.site.register(Listing)
+admin.site.register(Payment)
