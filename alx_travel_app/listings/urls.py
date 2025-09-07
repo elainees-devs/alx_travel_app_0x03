4beb5ea2bb369fc3b8aec24a0486fda66fa61728
elainees-devs <elainees.devs@gmail.com>
2025-09-07 18:45:03 +0300

    format code

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,17 +1,28 @@
 from django.urls import path, include
 from .create_superuser import create_admin
 from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet, VerifyPaymentView, VerifiedPaymentsView, test_send_email
+from .views import (
+    ListingViewSet,
+    BookingViewSet,
+    VerifyPaymentView,
+    VerifiedPaymentsView,
+    test_send_email,
+)
 
 router = DefaultRouter()
-router.register(r'listings', ListingViewSet, basename='listing')
-router.register(r'bookings', BookingViewSet, basename='booking')
+router.register(r"listings", ListingViewSet, basename="listing")
+router.register(r"bookings", BookingViewSet, basename="booking")
 
 urlpatterns = [
-    path('', include(router.urls)),  # includes /bookings/{id}/pay/
-    path('payments/verify/<int:booking_id>/', VerifyPaymentView.as_view(), name='verify-payment'),
-    path('payments/verified/', VerifiedPaymentsView.as_view(), name='verified-payments'),
-    path('email/test-send-email/', test_send_email),
-     path("create-admin/", create_admin),
-
+    path("", include(router.urls)),  # includes /bookings/{id}/pay/
+    path(
+        "payments/verify/<int:booking_id>/",
+        VerifyPaymentView.as_view(),
+        name="verify-payment",
+    ),
+    path(
+        "payments/verified/", VerifiedPaymentsView.as_view(), name="verified-payments"
+    ),
+    path("email/test-send-email/", test_send_email),
+    path("create-admin/", create_admin),
 ]
