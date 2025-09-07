702be70740a04504786016215d13aa88df117003
elainees-devs <elainees.devs@gmail.com>
2025-09-07 18:06:15 +0300

    enable broker connection retry on startup

--- alx_travel_app/celery.py
+++ alx_travel_app/celery.py
@@ -6,3 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
 app = Celery('alx_travel_app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.broker_connection_retry_on_startup = True
