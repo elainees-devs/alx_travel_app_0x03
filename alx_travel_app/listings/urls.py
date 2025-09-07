1d571421b099cbc6e50be19db756852f3e159986
elainees-devs <elainees.devs@gmail.com>
2025-09-07 18:40:18 +0300

    add temporary create-admin endpoint for Render superuser setup

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from .create_superuser import create_admin
 from rest_framework.routers import DefaultRouter
 from .views import ListingViewSet, BookingViewSet, VerifyPaymentView, VerifiedPaymentsView, test_send_email
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('payments/verify/<int:booking_id>/', VerifyPaymentView.as_view(), name='verify-payment'),
     path('payments/verified/', VerifiedPaymentsView.as_view(), name='verified-payments'),
     path('email/test-send-email/', test_send_email),
+     path("create-admin/", create_admin),
+
 ]
