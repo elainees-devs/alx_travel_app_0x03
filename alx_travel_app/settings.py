c86ca07d37c9b4697d34cded998fb7a02ca1c147
elainees-devs <elainees.devs@gmail.com>
2025-09-07 17:14:33 +0300

    remove typo in alx-travel-app-rinx.onrender.com in ALLOWED_HOSTS

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -20,7 +20,7 @@ BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
 DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = [
-    "alx-travel-app-rinx.onrender.com'",
+    "alx-travel-app-rinx.onrender.com",
     "127.0.0.1",
     "localhost",
     "testserver",
